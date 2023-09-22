ae07c3f31094805024a0b8ede26c4e063610d1c6
Arthur Schmidt <arthur.schmidt@duque.g12.br>
2023-09-21 21:32:21 -0300

    primeiro commit

---
+++ src/main/java/br/g12/duque/arthur/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.g12.duque.arthur.calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 08063
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setBackground(Color.black);
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+}
